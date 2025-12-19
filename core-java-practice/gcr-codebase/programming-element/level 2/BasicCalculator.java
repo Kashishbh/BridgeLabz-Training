049eaeb83e5428f4d0ca3da68743d7baa534e466
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 11:13:11 +0530

    Update and rename Basic_Calculator.java to BasicCalculator.java

--- core-java-practice/gcr-codebase/programming-element/level 2/BasicCalculator.java
+++ core-java-practice/gcr-codebase/programming-element/level 2/BasicCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Basic_Calculator {
+public class BasicCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
