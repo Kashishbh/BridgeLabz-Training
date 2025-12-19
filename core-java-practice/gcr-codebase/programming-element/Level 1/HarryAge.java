422282722a8ff05f0281174782445174734d688b
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 11:06:14 +0530

    Update and rename Harry_age.java to HarryAge.java

---
+++ core-java-practice/gcr-codebase/programming-element/Level 1/HarryAge.java
@@ -0,0 +1,8 @@
+public class HarryAge {
+    public static void main(String[] args) {
+        int birthYear=2000;
+        int currYear=2024;
+        int harryAge= currYear-birthYear;
+        System.out.println(harryAge);
+    }
+}
--- core-java-practice/gcr-codebase/programming-element/Level 1/Harry_age.java
+++
@@ -1,8 +0,0 @@
-public class Harry_age {
-    public static void main(String[] args) {
-        int birth_year=2000;
-        int curr_year=2024;
-        int harry_age= curr_year-birth_year;
-        System.out.println(harry_age);
-    }
-}
