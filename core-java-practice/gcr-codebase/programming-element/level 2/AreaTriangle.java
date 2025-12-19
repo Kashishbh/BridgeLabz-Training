d03c8992eb3645cccbb726181865b02c98492c38
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 11:12:35 +0530

    Update and rename Area_of_Triangle.java to AreaTriangle.java

--- core-java-practice/gcr-codebase/programming-element/level 2/AreaTriangle.java
+++ core-java-practice/gcr-codebase/programming-element/level 2/AreaTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Area_of_Triangle {
+public class AreaTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
