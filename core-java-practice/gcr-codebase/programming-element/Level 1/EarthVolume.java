1575f222a4a98568fbba02b2b8a081bbec64254b
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 11:00:18 +0530

    Update and rename Earth_volume.java to EarthVolume.java

--- core-java-practice/gcr-codebase/programming-element/Level 1/EarthVolume.java
+++ core-java-practice/gcr-codebase/programming-element/Level 1/EarthVolume.java
@@ -1,9 +1,9 @@
-public class Earth_volume {
+public class EarthVolume {
     public static void main(String[] args) {
-        double r = 6378;
+        double ra = 6378;
         double pi = Math.PI;
 
-        double volume_Km = (4.0 / 3) * pi * Math.pow(r, 3);
+        double volume_Km = (4.0 / 3) * pi * Math.pow(ra, 3);
         double volume_Miles = volume_Km / Math.pow(1.609, 3);
 
         System.out.println("The volume of earth in cubic kilometers is " +
