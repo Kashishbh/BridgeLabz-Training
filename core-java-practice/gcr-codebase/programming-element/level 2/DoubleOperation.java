f58f9ccfd186e75d2ca5beb89acd1d7261d55b4b
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 11:17:04 +0530

    Update DoubleOperation.java

--- core-java-practice/gcr-codebase/programming-element/level 2/DoubleOperation.java
+++ core-java-practice/gcr-codebase/programming-element/level 2/DoubleOperation.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 public class DoubleOperation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        double a, b, c;
-
-        System.out.print("Enter value of a: ");
-        a = sc.nextDouble();
-
-        System.out.print("Enter value of b: ");
-        b = sc.nextDouble();
-
-        System.out.print("Enter value of c: ");
-        c = sc.nextDouble();
-
-        double result1 = a + b * c;
-        double result2 = a * b + c;
-        double result3 = c + a / b;
-        double result4 = a % b + c;
+        int val1 = sc.nextDouble();
+        int val2 = sc.nextDouble();
+        int val3 = sc.nextDouble();
+        double result1 = val1 + val2 * val3;
+        double result2 = val1 * val2 + val3;
+        double result3 = val3 + val1 / val2;
+        double result4 = val1 % val2 + val3;
 
         System.out.println("The results of Double Operations are "
                 + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
