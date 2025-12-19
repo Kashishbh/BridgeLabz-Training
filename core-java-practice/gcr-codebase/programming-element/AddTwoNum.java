1b47f4a6e73bbaf303f32f9c13a5a4a9576f1b0a
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 10:50:35 +0530

    Update and rename Add_two_num.java to AddTwoNum.java

--- core-java-practice/gcr-codebase/programming-element/AddTwoNum.java
+++ core-java-practice/gcr-codebase/programming-element/AddTwoNum.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-class Add_two_num {
+class AddTwoNum {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int val1= sc.nextInt();
+        int val2 = sc.nextInt();
 
-        System.out.println(a + b);
+        System.out.println(val1 + val2);
     }
 }
