c64700b62cffb72e8a2aeeb90b30cab2a0a45c08
Kashishbh <152801327+Kashishbh@users.noreply.github.com>
2025-12-19 11:19:23 +0530

    Update IntOperation.java

--- core-java-practice/gcr-codebase/programming-element/level 2/IntOperation.java
+++ core-java-practice/gcr-codebase/programming-element/level 2/IntOperation.java
@@ -3,22 +3,13 @@ import java.util.Scanner;
 public class IntOperation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a, b, c;
-
-        System.out.print("Enter value of a: ");
-        a = sc.nextInt();
-
-        System.out.print("Enter value of b: ");
-        b = sc.nextInt();
-
-        System.out.print("Enter value of c: ");
-        c = sc.nextInt();
-
-        int r1 = a + b * c;
-        int r2 = a * b + c;
-        int r3 = c + a / b;
-        int r4 = a % b + c;
+        int v1 = sc.nextInt();
+        int v2 = sc.nextInt();
+        int v3 = sc.nextInt();
+        int r1 = v1 + v2 * v3;
+        int r2 = v1 * v2 + v3;
+        int r3 = v3 + v1 / v2;
+        int r4 = v1 % v2 + v3;
 
         System.out.println("The results of Int Operations are "
                 + r1 + ", " + r2 + ", " + r3 + ", and " + r4);
