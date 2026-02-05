099f6574c88afcee0c709e004ae2882aa07bdb09
kashish bhatnagar <kas.bhatnagar@gmail.com>
2026-02-05 21:43:27 +0530

    [kashish] Solved AeroVigil Problem

---
+++ dsa-practice/scenario-based/aeroVigil/Main.java
@@ -0,0 +1,30 @@
+package aeroVigil;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        Flight util=new Flight();
+        System.out.println("Enter flight details");
+        try {
+            String input=scan.nextLine();
+            String[] data=input.split(":");
+            String flightNumber=data[0];
+            String flightName=data[1];
+            int passengerCount=Integer.parseInt(data[2]);
+            double currentFuel=Double.parseDouble(data[3]);
+            util.validateFlightNumber(flightNumber);
+            util.validateFlightName(flightName);
+            util.validatePassengerCount(passengerCount, flightName);
+            double fuelRequired = util.calculateFuelToFillTank(flightName, currentFuel);
+            System.out.println("Fuel required to fill the tank -> "+fuelRequired+" litres");
+        }
+        catch (InvalidFlightException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
